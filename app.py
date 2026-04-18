93b192d05ff6620c68f18d4f8cd11d0b4c033ccb
aishwaryaumak24-byte <aishwarya.umak24@vit.edu>
2026-04-18 16:54:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "autism_model.pkl")
 COLUMNS_PATH = os.path.join(BASE_DIR, "columns.json")
 
 app = Flask(__name__)
